82ed7d02a956cf9c6ef600d54841e4e22c29ac1a
hiendt-0025 <dao.thi.hien@sun-asterisk.com>
2019-07-30 13:33:05 +0700

    manage place city

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  scope :admin do
-    root "admin#index"
-  end
-
   get "signup", to: "users#new"
   post "signup", to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
   resources :users do
     member do
       get :following, :followers
@@ -17,4 +14,9 @@ Rails.application.routes.draw do
   resources :relationships, only: %i(create destroy)
   resources :reactions, only: %i(show create destroy)
   resources :comments, only: %i(show create destroy)
+
+  namespace :admin do
+    root "/admin#index"
+    resources :cities
+  end
 end
